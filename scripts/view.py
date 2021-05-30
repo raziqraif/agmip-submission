a078ff10ccc7081224d62f994db3ce28027be388
Raziq Raif Ramli <42981908+raziqraif@users.noreply.github.com>
2021-05-30 19:30:18 -0400

    Refactor UI codes in view

--- scripts/view.py
+++ scripts/view.py
@@ -53,64 +53,59 @@ class View:
         PAGE_TITLES = ["File Upload", "Data Specification", "Integrity Checking", "Plausibility Checking"]
         NUM_OF_PAGES = 4
 
-        # High level components
-        app_container = ui.VBox()
+        # Create header bar
         header_bar = ui.HTML(APP_TITLE)
-        body_container = ui.VBox()
-
-        # Widgets to manage app pages
-        self.stepper = ui.HBox()
-        self.page_container = ui.Box()
-        self.file_upload_page = self._build_file_upload_page()
-        self.data_specification_page = self._build_data_specification_page()
-        self.integrity_checkgin_page = self._build_integrity_checking_page()
-        self.integrity_checkgin_page = self._build_plausibility_checking_page()
-
-        # Assign layouts & styling
-        app_container.layout = ui.Layout(
-            width="100%", height="912px", border="1px solid " + DARK_BLUE, padding="0px 0px"
-        )
         header_bar.add_class("c-header-bar")
-        body_container.layout = ui.Layout(flex="1", align_items="center", padding="36px 48px")
-        self.page_container.layout = ui.Layout(flex="1", width="100%")
-        self.stepper.add_class("c-stepper")
 
-        # Assign children widgets
-        app_container.children = [header_bar, body_container]
-        body_container.children = [self.stepper, self.page_container]
-        self.page_container.children = [self.file_upload_page]
-
-        # Create & assign stepper children
+        # Create stepper
         stepper_children = []
         for i in range(0, NUM_OF_PAGES):
             page_number = ui.HTML(value=str(i + 1))
-            separator = ui.HTML("<hr width=48px/>")
-            page_title = ui.HTML(PAGE_TITLES[i])
-
-            page_number.add_class("c-stepper__number")
-
-            # Available modifiers are current, active, inactive
+            page_number.add_class("c-stepper__number")  # Available modifiers are current, active, inactive
             page_number.add_class("c-stepper__number--current" if i == 0 else "c-stepper__number--inactive")
-            # Available modifiers are active, inactive
+
+            page_title = ui.HTML(PAGE_TITLES[i])  # Available modifiers are active, inactive
             page_title.add_class("c-stepper__title--active" if i == 0 else "c-stepper__title--inactive")
+            separator = ui.HTML("<hr width=48px/>")  # Available modifiers are active, inactive
             separator.add_class("c-stepper__separator--inactive")
 
             is_last_page = i == NUM_OF_PAGES - 1
             stepper_children += [page_number, page_title] if is_last_page else [page_number, page_title, separator]
+        self.stepper = ui.HBox(stepper_children)
+        self.stepper.add_class("c-stepper")
 
-        self.stepper.children = stepper_children
+        # Create app pages & page container
+        self.file_upload_page = self._build_file_upload_page()
+        self.data_specification_page = self._build_data_specification_page()
+        self.integrity_checkgin_page = self._build_integrity_checking_page()
+        self.integrity_checkgin_page = self._build_plausibility_checking_page()
+        self.page_container = ui.Box(
+            [self.file_upload_page], layout=ui.Layout(flex="1", width="100%")  # page container stores the current page
+        )
 
-        return app_container
+        return ui.VBox(  # app container
+            [
+                header_bar,
+                ui.VBox(  # -body container
+                    children=[self.stepper, self.page_container],
+                    layout=ui.Layout(flex="1", align_items="center", padding="36px 48px"),
+                ),
+            ],
+            layout=ui.Layout(width="100%", height="912px", border="1px solid " + DARK_BLUE, padding="0px 0px"),
+        )
 
     def _build_file_upload_page(self) -> ui.Box:
         INSTRUCTION = '<h3 style="margin: 0px;">Upload file to be processed</h3>'
-        SUB_INSTRUCTION = '<span style="font-size: 15px; line-height: 20px; margin: 0px; color: var(--grey);">File should be in CSV format</span>'
+        SUB_INSTRUCTION = (
+            '<span style="font-size: 15px; line-height: 20px; margin: 0px; color: var(--grey);">File should be in CSV'
+            " format</span>"
+        )
         UPLOADED_FILE = '<div style="width: 125px; line-height: 36px;">Uploaded file</div>'
         SAMPLE_FILE = '<div style="width: 125px; line-height: 36px;">Sample file</div>'
-        NO_FILES_UPLOADED = '<div \
-            style="width: 365px; line-height: 36px; border-radius: 4px; padding: 0px 16px; background: var(--light-grey); color: white;">\
-            No files uploaded\
-        </div>'
+        NO_FILES_UPLOADED = (
+            '<div style="width: 365px; line-height: 36px; border-radius: 4px; padding: 0px 16px;'
+            ' background: var(--light-grey); color: white;"> No files uploaded </div>'
+        )
 
         # Create file upload area
         upload_area_bg = ui.Box(
@@ -145,39 +140,25 @@ class View:
             description="Next", disabled=True, layout=ui.Layout(align_self="flex-end", justify_self="flex-end")
         )
 
-        return ui.VBox(
+        return ui.VBox(  # page
             [
-                ui.VBox(
+                ui.VBox(  # -container to fill up the space above navigation button area
                     [
-                        ui.VBox(
-                            layout=ui.Layout(width="500px"),
-                            children=[
-                                ui.HTML(INSTRUCTION),
-                                ui.HTML(SUB_INSTRUCTION),
-                            ],
+                        ui.VBox(  # --instruction container
+                            layout=ui.Layout(width="500px"), children=[ui.HTML(INSTRUCTION), ui.HTML(SUB_INSTRUCTION)]
                         ),
                         upload_area,
-                        ui.HBox(
-                            [
-                                ui.HTML(UPLOADED_FILE),
-                                self.uploaded_file_snackbar,
-                                # ui.HTML(NO_FILES_UPLOADED),
-                            ],
+                        ui.HBox(  # --uploaded file container
+                            [ui.HTML(UPLOADED_FILE), self.uploaded_file_snackbar],
                             layout=ui.Layout(width="500px", margin="0px 0px 4px 0px"),
                         ),
-                        ui.HBox(
-                            [ui.HTML(SAMPLE_FILE), download_button],
-                            layout=ui.Layout(width="500px"),
+                        ui.HBox(  # --sample file container
+                            [ui.HTML(SAMPLE_FILE), download_button], layout=ui.Layout(width="500px")
                         ),
                     ],
                     layout=ui.Layout(flex="1", justify_content="center"),
                 ),
-                ui.VBox(
-                    [
-                        self.next_button,
-                    ],
-                    layout=ui.Layout(align_self="flex-end"),
-                ),
+                ui.VBox([self.next_button], layout=ui.Layout(align_self="flex-end")),  # -container for navgation button
             ],
             layout=ui.Layout(flex="1", width="100%", align_items="center", justify_content="center"),
         )
