3a34bac768c4ba96d17ba85eff320cc8c12505bd
Raziq Ramli <42981908+raziqraif@users.noreply.github.com>
2021-06-06 03:48:50 -0400

    Test file upload api

--- tests/test_nbserver.py
+++ tests/test_nbserver.py
@@ -5,26 +5,28 @@ from typing import Optional
 import urllib
 import pytest
 from pathlib import Path
+import requests
 
 
 @pytest.fixture
 def nbserver_raw_info() -> Optional[str]:
     """
-    format:
-    nbserver_raw_info -> nbserver_url :: launch_dirpath
-    nbserver_url -> nbserver_base_url/?token=token
-    launch_dirpath -> /../../launch_dir
+    Format:
+    NBSERVER_RAW_INFO -> NBSERVER_URL :: LAUNCH_DIRPATH
+    NBSERVER_URL -> NBSERVER_BASE_URL/?token=TOKEN
+    LAUNCH_DIRPATH -> /../../LAUNCH_DIR
     """
 
-    stream = os.popen("jupyter notebook list")
+    # Conda activation doesn't work well with vscode, so hard code this for now
+    stream = os.popen("/Users/raziqraif/opt/anaconda3/envs/agmip/bin/jupyter notebook list")
 
-    # output -> <title>:\nnbserver_raw_info\nnbserver_raw_info\n...
+    # output -> <title>:\nNBSERVER_RAW_INFO\nNBSERVER_RAW_INFO\n...
     output: str = stream.read()
 
-    if "http" not in output:  # no server is running
+    if "http" not in output:  # No server is running
         return None
 
-    # output -> nbserver_raw_info
+    # output -> NBSERVER_RAW_INFO
     output = output.split("\n")[1]  # assume our nbserver is at the top
     return output
 
@@ -33,7 +35,21 @@ def nbserver_raw_info() -> Optional[str]:
 def nbserver_url(nbserver_raw_info) -> Optional[str]:
     if nbserver_raw_info:
         return nbserver_raw_info.split(" :: ")[0]
-    return None
+    return None  # No server is running
+
+
+@pytest.fixture
+def nbserver_baseurl(nbserver_url) -> Optional[str]:
+    if nbserver_url:
+        return nbserver_url.split("/?token=")[0]
+    return None  # No server is running
+
+
+@pytest.fixture
+def nbserver_token(nbserver_url) -> Optional[str]:
+    if nbserver_url:
+        return nbserver_url.split("/?token=")[1]
+    return None  # No server is running
 
 
 @pytest.fixture
@@ -44,46 +60,62 @@ def launch_dirpath(nbserver_raw_info) -> Optional[str]:
 
 
 def test_nbserver_launch_dirpath(launch_dirpath):
-    """ If notebook server is not launched from proj directory, file upload to workingdir/ will fail """
+    """
+    If notebook server is not launched from proj directory, file upload will fail
+    """
 
     project_dir: Path = Path(__file__).parent.parent
-    if launch_dirpath is None:  # no server is running
+    if launch_dirpath is None:  # No server is running
         return
     launch_dir: Path = Path(launch_dirpath)
     assert launch_dir == project_dir
 
 
-def test_file_upload_api(nbserver_url):
-    # def jupyter_upload(token, filepath, resourcedstpath, jupyterurl='http://localhost:8888'):
-    """
-    uploads file to jupyter notebook server
-    ----------------------------------------
-    :param token:
-        the authorization token issued by jupyter for authentification
-        (enabled by default as of version 4.3.0)
-    :param filepath:
-        the file path to the local content to be uploaded
+def test_jupyter_file_upload_api(nbserver_baseurl, nbserver_token):
+    if nbserver_url is None:  # No server is running
+        return
 
-    :param resourcedstpath:
-        the path where resource should be placed.
-        the destination directory must exist.
+    # Prepare file to upload
 
-    :param jupyterurl:
-        the url to the jupyter server. default value is typical localhost installation.
+    TEMP_FILE_NAME = "abcdefghij123"
+    if Path(TEMP_FILE_NAME).exists():
+        Path(TEMP_FILE_NAME).unlink()
 
-    :return: server response
-    """
-    pass
-    # file_destination_path = "workingdir/testfile.delete"
-    # dstpath = urllib.quote(resourcedstpath)
-    # dsturl = "%s/api/contents/%s" % (jupyterurl, dstpath)
-    # filename = filepath[1 + filepath.rfind(os.sep) :]
-    # headers = {}
-    # headers["authorization"] = "token " + token
-    # with open(filepath, "r") as myfile:
-    #     data = myfile.read()
-    #     b64data = base64.encodestring(data)
-    #     body = json.dumps(
-    #         {"content": b64data, "name": filename, "path": resourcedstpath, "format": "base64", "type": "file"}
-    #     )
-    #     return requests.put(dsturl, data=body, headers=headers, verify=true)
+    temp_file_path = Path(TEMP_FILE_NAME)
+    temp_file = open(TEMP_FILE_NAME, "w+")
+    assert temp_file
+
+    uploads_dirpath = Path(__name__).parent.parent / Path("uploads")  # <project_dir>/uploads
+    uploaded_temp_filepath = Path(uploads_dirpath / TEMP_FILE_NAME)
+
+    if uploaded_temp_filepath.exists():
+        uploaded_temp_filepath.unlink()
+    assert uploaded_temp_filepath.exists() == False
+
+    # Upload file
+
+    file_destination_path = "uploads/" + TEMP_FILE_NAME
+    url = nbserver_baseurl + "/api/contents/" + file_destination_path
+    print("url =", url)
+    headers = {}
+    headers["authorization"] = "token " + nbserver_token
+    file_content = temp_file.read()
+    file_content = file_content.encode("ascii")
+    base64_file_content = base64.b64encode(file_content)
+    base64_file_content = base64_file_content.decode("ascii")
+
+    body = json.dumps(
+        {
+            "content": base64_file_content,
+            "name": TEMP_FILE_NAME,
+            "path": file_destination_path,
+            "format": "base64",
+            "type": "file",
+        }
+    )
+    response = requests.put(url, data=body, headers=headers, verify=True)
+    
+    assert response.status_code == 201
+    assert uploaded_temp_filepath.exists() == True
+    temp_file_path.unlink()
+    uploaded_temp_filepath.unlink()
