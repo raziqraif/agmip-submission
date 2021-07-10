54b738977fe9f8e1d1a99578018a25616608340c
Raziq Ramli <42981908+raziqraif@users.noreply.github.com>
2021-07-10 13:18:42 -0400

    Minor update to profiler

--- tests/profile_data_cleaning_service.py
+++ tests/profile_data_cleaning_service.py
@@ -22,6 +22,15 @@ data_specification.guess_header_is_included()
 data_specification.guess_model_name_n_column_assignments()
 
 def run_cleaning_service():
+    # Init data spec
+    data_specification = DataSpecification()
+    data_specification.uploaded_filepath = (
+        Model.WORKING_DIR.parent / "resources" / "submissions" / "output_GLOBIOM_AgMIP3_12mar2021.csv"
+    )
+    data_specification.load_file()
+    data_specification.delimiter = ","
+    data_specification.guess_header_is_included()
+    data_specification.guess_model_name_n_column_assignments()
     data_cleaner = DataCleaningService(data_specification)
     data_cleaner.parse_data()
 
