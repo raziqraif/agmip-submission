ecfcb8cbc061d70ce9fbf5ca896126d72c527f52
Raziq Raif Ramli <42981908+raziqraif@users.noreply.github.com>
2021-08-06 09:21:05 -0400

    Update tests

--- tests/test_input_data_diagnosis.py
+++ tests/test_input_data_diagnosis.py
@@ -8,7 +8,7 @@ from typing import List
 # Modify PATH so that the following imports work
 sys.path.insert(0, os.path.dirname("scripts"))
 from scripts.model import Model
-from scripts.business import DataSpecification, DataCleaningService
+from scripts.domain import InputDataDiagnosis, InputDataEntity, OutputDataEntity
 
 
 """
@@ -47,42 +47,36 @@ def print_test_file(filepath: Path) -> None:
         print(file.readlines())
 
 
-@pytest.fixture
-def data_specification() -> DataSpecification:
-    """
-    Return arguments required to initialize integrity checking states
-    The arguments are based on SAMPLE_RAW_CSV
-    """
-    spec = DataSpecification()
-    spec.delimiter = ","
-    spec.header_is_included = False
-    spec.initial_lines_to_skip = 0
-    spec.scenarios_to_ignore = []
-    spec.model_name = "AIM"
-    spec.scenario_colnum = 1
-    spec.region_colnum = 2
-    spec.variable_colnum = 3
-    spec.item_colnum = 4
-    spec.unit_colnum = 6
-    spec.year_colnum = 5
-    spec.value_colnum = 7
-    return spec
+def populate_input_entity_based_on_sample_raw_csv(input_entity: InputDataEntity) -> None:
+    """ Populate input entity """
+    input_entity.delimiter = ","
+    input_entity.header_is_included = False
+    input_entity.initial_lines_to_skip = 0
+    input_entity.scenarios_to_ignore = []
+    input_entity.model_name = "AIM"
+    input_entity.scenario_colnum = 1
+    input_entity.region_colnum = 2
+    input_entity.variable_colnum = 3
+    input_entity.item_colnum = 4
+    input_entity.unit_colnum = 6
+    input_entity.year_colnum = 5
+    input_entity.value_colnum = 7
 
 
-def test_explore(data_specification: DataSpecification):
+def test_explore():
     """Test if duplicate rows are pruned correctly"""
     ROWS = [
         "SSP2_NoMt_NoCC_FlexA_WLD_2500,mEN,oTHU,VfN|VEG,2030,1000 t fm,151.8507839",  # NOSONAR
     ]
     filepath = create_test_file(ROWS)
-    data_specification.uploaded_filepath = filepath
-    data_cleaner = DataCleaningService(data_specification)
-    data_cleaner.parse_data()
-    assert data_cleaner.nrows_duplicate == len(ROWS) - 1
-    assert data_cleaner.nrows_accepted == 1
+    input_entity = InputDataEntity.create(filepath)
+    populate_input_entity_based_on_sample_raw_csv(input_entity)
+    diagnosis = InputDataDiagnosis.create(input_entity)
+    assert diagnosis.nrows_duplicate == len(ROWS) - 1
+    assert diagnosis.nrows_accepted == 1
 
 
-def test_duplicate_rows(data_specification: DataSpecification):
+def test_duplicate_rows():
     """Test if duplicate rows are pruned correctly"""
     ROWS = [
         "SSP2_NoMt_NoCC_FlexA_WLD_2500,MEN,OTHU,VFN|VEG,2030,1000 t fm,151.8507839",  # NOSONAR
@@ -91,14 +85,14 @@ def test_duplicate_rows(data_specification: DataSpecification):
         "SSP2_NoMt_NoCC_FlexA_WLD_2500,MEN,OTHU,VFN|VEG,2030,1000 t fm,151.8507839",
     ]
     filepath = create_test_file(ROWS)
-    data_specification.uploaded_filepath = filepath
-    data_cleaner = DataCleaningService(data_specification)
-    data_cleaner.parse_data()
-    assert data_cleaner.nrows_duplicate == len(ROWS) - 1
-    assert data_cleaner.nrows_accepted == 1
+    input_entity = InputDataEntity.create(filepath)
+    populate_input_entity_based_on_sample_raw_csv(input_entity)
+    diagnosis = InputDataDiagnosis.create(input_entity)
+    assert diagnosis.nrows_duplicate == len(ROWS) - 1
+    assert diagnosis.nrows_accepted == 1
 
 
-def test_rows_w_structural_issue(data_specification: DataSpecification):
+def test_rows_w_structural_issue():
     """Test if rows with field issues are pruned correctly"""
     ROWS = [
         "SSP2_NoMt_NoCC_FlexA_DEV,CAN,CONS,RIC,2010,1000 t dm,162.6840595",
@@ -116,14 +110,14 @@ def test_rows_w_structural_issue(data_specification: DataSpecification):
         "row with missing field 2,,,,,,",
     ]
     filepath = create_test_file(ROWS)
-    data_specification.uploaded_filepath = filepath
-    data_cleaner = DataCleaningService(data_specification)
-    data_cleaner.parse_data()
-    assert data_cleaner.nrows_w_struct_issue == 5
-    assert data_cleaner.nrows_accepted == len(ROWS) - 5
+    input_entity = InputDataEntity.create(filepath)
+    populate_input_entity_based_on_sample_raw_csv(input_entity)
+    diagnosis = InputDataDiagnosis.create(input_entity)
+    assert diagnosis.nrows_w_struct_issue == 5
+    assert diagnosis.nrows_accepted == len(ROWS) - 5
 
 
-def test_rows_with_ignored_scenario(data_specification: DataSpecification):
+def test_rows_with_ignored_scenario():
     """Test if rows with an ignored scenario are pruned correctly"""
     ROWS = [
         "ignored scenario 1,CAN,CONS,RIC,2010,1000 t dm,162.6840595",
@@ -137,49 +131,38 @@ def test_rows_with_ignored_scenario(data_specification: DataSpecification):
         "SSP2_NoMt_NoCC_FlexA_WLD_2500,MEN,OTHU,VFN|VEG,2030,1000 t fm,151.8507839",
     ]
     ignored_scenarios = ["ignored scenario 1", "ignored scenario 2"]
-    data_specification.scenarios_to_ignore = ignored_scenarios
     filepath = create_test_file(ROWS)
-    data_specification.uploaded_filepath = filepath
-    data_cleaner = DataCleaningService(data_specification)
-    data_cleaner.parse_data()
-    assert data_cleaner.nrows_w_ignored_scenario == len(ignored_scenarios)
-    assert data_cleaner.nrows_accepted == len(ROWS) - len(ignored_scenarios)
+    input_entity = InputDataEntity.create(filepath)
+    populate_input_entity_based_on_sample_raw_csv(input_entity)
+    input_entity.scenarios_to_ignore = ignored_scenarios
+    diagnosis = InputDataDiagnosis.create(input_entity)
+    assert diagnosis.nrows_w_ignored_scenario == len(ignored_scenarios)
+    assert diagnosis.nrows_accepted == len(ROWS) - len(ignored_scenarios)
 
 
-def test_bad_labels(data_specification: DataSpecification) -> None:
+def test_bad_labels() -> None:
     """Test if bad labels are identified correctly"""
     ROWS = [
         "ssp2_nomt_nocc_flexa_dev,Can,cons,ric,2020,1000 T dm,#DIV/0!",
         "SSP2_NoMt_NoCC_FlexA_DEV,World,CONS,RIC,2030,1000 t dm,NA",
     ]
-    bad_labels = [
-        "ssp2_nomt_nocc_flexa_dev",
-        "Can",
-        "cons",
-        "ric",
-        "1000 T dm",
-        "#DIV/0!",
-        "NA",
-        "World"
-    ]
+    bad_labels = ["ssp2_nomt_nocc_flexa_dev", "Can", "cons", "ric", "1000 T dm", "#DIV/0!", "NA", "World"]
     fixed_labels = ["SSP2_NoMt_NoCC_FlexA_DEV", "CAN", "CONS", "RIC", "1000 t dm", "0", "0", "WLD"]
-    data_specification.uploaded_filepath = create_test_file(ROWS)
-    data_cleaner = DataCleaningService(data_specification)
-    data_cleaner.parse_data()
-    print(data_cleaner.bad_labels_table)
+    input_entity = InputDataEntity.create(create_test_file(ROWS))
+    populate_input_entity_based_on_sample_raw_csv(input_entity)
+    diagnosis = InputDataDiagnosis.create(input_entity)
+    print(diagnosis.bad_labels)
     for label in bad_labels:
         print(label)
     for label in fixed_labels:
         print(label)
     # TODO: do more fine-grained assertions
-    assert len(data_cleaner.bad_labels_table) == len(bad_labels)
+    assert len(diagnosis.bad_labels) == len(bad_labels)
 
 
-def test_unknown_labels(data_specification: DataSpecification) -> None:
+def test_unknown_labels() -> None:
     """Test if unknown labels are identified correctly"""
-    ROWS = [
-        "SSP2_NoMt_NoCC_FlexA_XYZW,MYXW,YIELDX,RICEX,2010,1000 pxyz dm,162.6840595"
-    ]
+    ROWS = ["SSP2_NoMt_NoCC_FlexA_XYZW,MYXW,YIELDX,RICEX,2010,1000 pxyz dm,162.6840595"]
     unknown_labels = [
         "SSP2_NoMt_NoCC_FlexA_XYZW",
         "MYXW",
@@ -187,12 +170,8 @@ def test_unknown_labels(data_specification: DataSpecification) -> None:
         "RICEX",
         "1000 pxyz dm",
     ]
-    data_specification.uploaded_filepath = create_test_file(ROWS)
-    data_cleaner = DataCleaningService(data_specification)
-    data_cleaner.parse_data()
-    print(data_cleaner.bad_labels_table)
-    for label in unknown_labels:
-        print(label)
-    for row in data_cleaner.unknown_labels_table:
-        print(row)
-    assert len(data_cleaner.unknown_labels_table) == len(unknown_labels)
+    input_entity = InputDataEntity.create(create_test_file(ROWS))
+    populate_input_entity_based_on_sample_raw_csv(input_entity)
+    diagnosis = InputDataDiagnosis.create(input_entity)
+    print(diagnosis.bad_labels)
+    assert len(diagnosis.unknown_labels) == len(unknown_labels)
--- tests/test_rule_gateway.py
+++ tests/test_rule_gateway.py
@@ -1,44 +1,44 @@
 import math
 
-from scripts.dataaccess import RuleGateway
+from scripts.domain import DataRuleRepository
 
 
 def test_fix_queries():
-    assert RuleGateway.query_fix_from_value_fix_table("NA") == "0"
-    assert RuleGateway.query_fix_from_value_fix_table("na") == "0"
-    assert RuleGateway.query_fix_from_value_fix_table("#DIV/0!") == "0"
-    assert RuleGateway.query_fix_from_region_fix_table("World") == "WLD"
-    assert RuleGateway.query_fix_from_region_fix_table("world") == "WLD"
+    assert DataRuleRepository.query_fix_from_value_fix_table("NA") == "0"
+    assert DataRuleRepository.query_fix_from_value_fix_table("na") == "0"
+    assert DataRuleRepository.query_fix_from_value_fix_table("#DIV/0!") == "0"
+    assert DataRuleRepository.query_fix_from_region_fix_table("World") == "WLD"
+    assert DataRuleRepository.query_fix_from_region_fix_table("world") == "WLD"
 
 
 def test_matching_queries():
-    assert RuleGateway.query_matching_scenario("ssp2_nomt_nocc_FLEXA_DEV") == "SSP2_NoMt_NoCC_FlexA_DEV"
-    assert RuleGateway.query_matching_region("Wld") == "WLD"
+    assert DataRuleRepository.query_matching_scenario("ssp2_nomt_nocc_FLEXA_DEV") == "SSP2_NoMt_NoCC_FlexA_DEV"
+    assert DataRuleRepository.query_matching_region("Wld") == "WLD"
     # Matching queries should not return result from the fix table
-    assert RuleGateway.query_matching_region("world") != "WLD"
-    assert RuleGateway.query_matching_variable("Cons") == "CONS"
-    assert RuleGateway.query_matching_variable("oTHU") == "OTHU"
-    assert RuleGateway.query_matching_item("Vfn|Veg") == "VFN|VEG"
-    assert RuleGateway.query_matching_unit("1000 T dm") == "1000 t dm"
+    assert DataRuleRepository.query_matching_region("world") != "WLD"
+    assert DataRuleRepository.query_matching_variable("Cons") == "CONS"
+    assert DataRuleRepository.query_matching_variable("oTHU") == "OTHU"
+    assert DataRuleRepository.query_matching_item("Vfn|Veg") == "VFN|VEG"
+    assert DataRuleRepository.query_matching_unit("1000 T dm") == "1000 t dm"
 
 
 def test_partially_matching_queries():
-    scenarios = RuleGateway._scenarios
-    assert RuleGateway.query_partially_matching_scenario("dummy_label") in scenarios
-    variables = RuleGateway._variables
-    assert RuleGateway.query_partially_matching_variable("dummy_label") in variables
-    items = RuleGateway._items
-    assert RuleGateway.query_partially_matching_item("dummy_label") in items
-    regions = RuleGateway._regions
-    assert RuleGateway.query_partially_matching_region("dummy_label") in regions
-    units = RuleGateway._units
-    assert RuleGateway.query_partially_matching_unit("dummy_label") in units
+    scenarios = DataRuleRepository._scenarios
+    assert DataRuleRepository.query_partially_matching_scenario("dummy_label") in scenarios
+    variables = DataRuleRepository._variables
+    assert DataRuleRepository.query_partially_matching_variable("dummy_label") in variables
+    items = DataRuleRepository._items
+    assert DataRuleRepository.query_partially_matching_item("dummy_label") in items
+    regions = DataRuleRepository._regions
+    assert DataRuleRepository.query_partially_matching_region("dummy_label") in regions
+    units = DataRuleRepository._units
+    assert DataRuleRepository.query_partially_matching_unit("dummy_label") in units
 
 
 def test_minimum_and_maximum_variable_value():
-    assert RuleGateway.query_variable_min_value("POPT", "million") > -1
-    assert RuleGateway.query_variable_max_value("POPT", "million") > 1000
-    assert RuleGateway.query_variable_min_value("ECH4", "MtCO2e") >= 0
-    assert RuleGateway.query_variable_max_value("ECH4", "MtCO2e") <= math.inf
-    assert RuleGateway.query_variable_min_value("YILD", "dm t/ha") >= 0
-    assert RuleGateway.query_variable_max_value("YILD", "fm t/ha") <= 1000
+    assert DataRuleRepository.query_variable_min_value("POPT", "million") > -1
+    assert DataRuleRepository.query_variable_max_value("POPT", "million") > 1000
+    assert DataRuleRepository.query_variable_min_value("ECH4", "MtCO2e") >= 0
+    assert DataRuleRepository.query_variable_max_value("ECH4", "MtCO2e") <= math.inf
+    assert DataRuleRepository.query_variable_min_value("YILD", "dm t/ha") >= 0
+    assert DataRuleRepository.query_variable_max_value("YILD", "fm t/ha") <= 1000
